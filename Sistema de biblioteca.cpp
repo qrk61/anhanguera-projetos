b86aa7bf21b29452136528fea4aedcd36a64c4ca
qrk61 <133074789+qrk61@users.noreply.github.com>
2023-06-04 11:59:24 -0300

    O construtor da classe Livro agora tem um parâmetro adicional quantidade que permite especificar a quantidade inicial de livros no estoque.
    No método adicionarLivro, agora é possível atualizar a quantidade de um livro existente no estoque se ele já estiver presente.
    
    No método emprestarLivro, agora é verificado se há disponibilidade do livro antes de emprestá-lo.
    
    No método devolverLivro, a quantidade do livro é incrementada quando o livro é devolvido.
    
    Foram adicionados os métodos pesquisarLivroPorTitulo e pesquisarLivroPorAutor para permitir a pesquisa de livros pelo título ou autor. Esses métodos exibem os resultados da pesquisa ou uma mensagem adequada se nenhum livro for encontrado.

--- Sistema de biblioteca.cpp
+++ Sistema de biblioteca.cpp
@@ -4,40 +4,52 @@
 
 using namespace std;
 
-// Classe para representar um livro
 class Livro {
 public:
     string titulo;
     string autor;
     bool emprestado;
+    int quantidade; // Nova adição: controla a quantidade de livros no estoque
 
-    Livro(string titulo, string autor) {
+    Livro(string titulo, string autor, int quantidade = 1) {
         this->titulo = titulo;
         this->autor = autor;
         this->emprestado = false;
+        this->quantidade = quantidade;
     }
 };
 
-// Classe para representar o sistema de biblioteca
 class SistemaBiblioteca {
 private:
     vector<Livro> estoque;
 
 public:
-    // Adicionar um novo livro ao estoque
-    void adicionarLivro(string titulo, string autor) {
-        Livro livro(titulo, autor);
+    void adicionarLivro(string titulo, string autor, int quantidade = 1) {
+        // Verifica se o livro já existe no estoque
+        for (Livro& livro : estoque) {
+            if (livro.titulo == titulo && livro.autor == autor) {
+                livro.quantidade += quantidade;
+                cout << "Livro atualizado com sucesso." << endl;
+                return;
+            }
+        }
+
+        // Se o livro não existir, adiciona um novo ao estoque
+        Livro livro(titulo, autor, quantidade);
         estoque.push_back(livro);
+        cout << "Livro adicionado com sucesso." << endl;
     }
 
-    // Emprestar um livro para um usuário
     void emprestarLivro(string titulo) {
         for (Livro& livro : estoque) {
             if (livro.titulo == titulo) {
                 if (livro.emprestado) {
                     cout << "O livro já está emprestado." << endl;
+                } else if (livro.quantidade == 0) {
+                    cout << "O livro não está disponível no momento." << endl;
                 } else {
                     livro.emprestado = true;
+                    livro.quantidade--;
                     cout << "Livro emprestado com sucesso." << endl;
                 }
                 return;
@@ -46,12 +58,12 @@ public:
         cout << "Livro não encontrado no estoque." << endl;
     }
 
-    // Devolver um livro ao estoque
     void devolverLivro(string titulo) {
         for (Livro& livro : estoque) {
             if (livro.titulo == titulo) {
                 if (livro.emprestado) {
                     livro.emprestado = false;
+                    livro.quantidade++;
                     cout << "Livro devolvido com sucesso." << endl;
                 } else {
                     cout << "O livro não está emprestado." << endl;
@@ -62,14 +74,52 @@ public:
         cout << "Livro não encontrado no estoque." << endl;
     }
 
-    // Mostrar informações sobre o estoque de livros
     void mostrarEstoque() {
         cout << "Estoque de livros:" << endl;
         for (const Livro& livro : estoque) {
             cout << "Título: " << livro.titulo << endl;
             cout << "Autor: " << livro.autor << endl;
+            cout << "Quantidade disponível: " << livro.quantidade << endl;
             cout << "Status: " << (livro.emprestado ? "Emprestado" : "Disponível") << endl;
             cout << "----------------------------" << endl;
+        }
+    }
+
+    // Novo método: pesquisar livro por título
+    void pesquisarLivroPorTitulo(string titulo) {
+        bool encontrado = false;
+        cout << "Resultados da pesquisa por título: " << titulo << endl;
+        for (const Livro& livro : estoque) {
+            if (livro.titulo == titulo) {
+                cout << "Título: " << livro.titulo << endl;
+                cout << "Autor: " << livro.autor << endl;
+                cout << "Quantidade disponível: " << livro.quantidade << endl;
+                cout << "Status: " << (livro.emprestado ? "Emprestado" : "Disponível") << endl;
+                cout << "----------------------------" << endl;
+                encontrado = true;
+            }
+        }
+        if (!encontrado) {
+            cout << "Nenhum livro encontrado com o título: " << titulo << endl;
+        }
+    }
+
+    // Novo método: pesquisar livro por autor
+    void pesquisarLivroPorAutor(string autor) {
+        bool encontrado = false;
+        cout << "Resultados da pesquisa por autor: " << autor << endl;
+        for (const Livro& livro : estoque) {
+            if (livro.autor == autor) {
+                cout << "Título: " << livro.titulo << endl;
+                cout << "Autor: " << livro.autor << endl;
+                cout << "Quantidade disponível: " << livro.quantidade << endl;
+                cout << "Status: " << (livro.emprestado ? "Emprestado" : "Disponível") << endl;
+                cout << "----------------------------" << endl;
+                encontrado = true;
+            }
+        }
+        if (!encontrado) {
+            cout << "Nenhum livro encontrado com o autor: " << autor << endl;
         }
     }
 };
@@ -78,7 +128,7 @@ int main() {
     SistemaBiblioteca sistema;
 
     sistema.adicionarLivro("Livro 1", "Autor 1");
-    sistema.adicionarLivro("Livro 2", "Autor 2");
+    sistema.adicionarLivro("Livro 2", "Autor 2", 3);
     sistema.adicionarLivro("Livro 3", "Autor 3");
 
     sistema.mostrarEstoque();
@@ -90,6 +140,9 @@ int main() {
     sistema.devolverLivro("Livro 2");
 
     sistema.mostrarEstoque();
+
+    sistema.pesquisarLivroPorTitulo("Livro 1");
+    sistema.pesquisarLivroPorAutor("Autor 2");
 
     return 0;
 }
